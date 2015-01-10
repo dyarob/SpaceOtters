05ef81cfcc0064848fdd48206a63ad708361b20f
dyarob <dyarob@gmail.com>
2015-01-10 19:00:12 +0100

    bordel

---
+++ test_list.cpp
@@ -0,0 +1,20 @@
+#include "List.struct.hpp"
+#include <iostream>
+#include <string>
+
+int		main( void )
+{
+	/*
+	AUnit	*a = new AUnit(1, 2, 3, 4, 5, NULL, NULL, NULL);
+	List	*test = new List(bleh);
+	List	*voila = new List(a, test);
+	voila = voila->push(bluh);
+	delete ( voila );
+	while (voila)
+	{
+		std::cout << *voila->o << std::endl;
+		voila = voila->next;
+	}
+	*/
+	return (0);
+}
