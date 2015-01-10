4fab9de1c49559ffdfd5d9b9f8f0316946ef3e3e
Jessy GIRARD <jgirard@e2r2p23.42.fr>
2015-01-10 22:41:59 +0100

    merge driss, jessy, stephane

---
+++ DelayEvent.class.cpp
@@ -0,0 +1,49 @@
+#include "DelayEvent.hpp"
+#include "EnemyBase.class.hpp"
+
+DelayEvent::DelayEvent(void) {
+	return;
+}
+
+DelayEvent::~DelayEvent(void){
+	return;
+}
+
+void DelayEvent::exec() {
+	// créer aléatoirement des chaines d'units
+
+	// recharger la weapon du vaisseau joueur
+
+
+}
+
+// génère une chaine d'ennemis aleatoire, d'une longueur aleatoire d'ennemis aleatoires.
+Event	&	DelayEvent::randomChain() {
+	static int randomWait = (std::rand() % (FRAME_MAX_TO_POP_CHAIN - FRAME_MIN_TO_POP_CHAIN)) + FRAME_MIN_TO_POP_CHAIN;
+	static int currentCount = 0;
+	currentCount++;
+	if (currentCount == randomWait) {
+		randomWait = (std::rand() % (FRAME_MAX_TO_POP_CHAIN - FRAME_MIN_TO_POP_CHAIN)) + FRAME_MIN_TO_POP_CHAIN;
+		currentCount = 0;
+		int chainLength = (std::rand() % (CHAIN_MAX_LENGTH - CHAIN_MIN_LENGTH)) + CHAIN_MIN_LENGTH;
+		int chainSpacing = (std::rand() % (CHAIN_MAX_SPACING - CHAIN_MIN_SPACING)) + CHAIN_MIN_SPACING;
+		int chainSpawnY = (std::rand() % (TEMP_WIN_Y - SPAWN_Y_PADDING * 2)) + SPAWN_Y_PADDING;
+		int enemyType = std::rand() % NUMBER_OF_ENEMY_TYPES;
+
+		for (int i = 0; i < chainLength; i++) {
+			lastEvent.pushBack(enemyType, (chainSpacing * (i + 1)), chainSpawnY);
+		}
+		return this->lastEvent;
+	}
+	return this->firstEvent;
+}
+
+// push un ennemi aléatoire dans la file d'attente
+void DelayEvent::createDelayedEnemy(int type, int delay, int spawn_y) {
+	lastEvent.pushBack(type, delay, spawn_y);
+}
+	
+std::ostream &		operator<<(std::ostream & o, DelayEvent const & rhs){
+	(void)rhs;
+	return o;
+}
---
+++ Event.class.cpp
@@ -0,0 +1,41 @@
+#include "Event.class.hpp"
+
+Event::Event() : _type(FIRSTNODE), _next(NULL), _prev(NULL) {
+}
+
+Event::Event(int type, int delay, int posY) : _type(type), _delay(delay), _posY(posY), _next(NULL), _prev(NULL) {
+}
+
+Event::Event(Event const & e) {
+	*this = e;
+}
+
+Event		&	Event::operator=(Event const & e) {
+	this->_type = e._type;
+	this->_delay = e._delay;
+	this->_posY = e._posY;
+	return *this;
+}
+
+int			Event::getType() const{
+	return this->_type;
+}
+int			Event::getDelay() const{
+	return this->_delay;
+}
+int			Event::getPosY() const{
+	return this->_posY;
+}
+
+void		Event::pushFront(int type, int delay, int posY) {
+	Event * n = new Event(type, delay, posY);
+	this->_prev = n;
+	n->_next = this;
+}
+void		Event::pushBack(int type, int delay, int posY) {
+	Event * n = new Event(type, delay, posY);
+	this->_next = n;
+	n->_prev = this;
+}
+
+Event::~Event() {}
