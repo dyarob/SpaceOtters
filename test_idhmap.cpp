1145dc4321842d626fa2ae7232ba43ffe8898247
dyarob <dyarob@gmail.com>
2015-11-10 16:56:39 +0100

    test_idhmap.cpp

--- test_idhmap.cpp
+++ test_idhmap.cpp
@@ -5,9 +5,11 @@
 int	main(int ac, char **av) {
 	d::initiate();
 	puts("INSERTION");
-	for (int i = 1; i < ac; i+=2) {
-		d::depth[i] = atoi(av[i]);
-		d::depth[i] = atoi(av[i+1]);
+	for (unsigned i = 1; i < ac; i+=2) {
+		//d::depth[i] = atoi(av[i]);
+		d::depth.emplace(i, atoi(av[i]));
+		//d::depth[i] = atoi(av[i+1]);
+		d::depth.emplace(i, atoi(av[i+1]));
 		printf("inserting %s with key %d\n", av[i], i);
 		printf("inserting %s with key %d\n", av[i+1], i);
 	}
@@ -16,6 +18,15 @@ int	main(int ac, char **av) {
 	for (d::multimap::iterator beg = d::depth.begin(); beg != end; ++beg) {
 		printf("key= %d value= %d\n", beg->first, beg->second);
 	}
-	puts("BUCKET TEST");
+	puts("UPPER/LOWER BOUND TEST");
+	for (unsigned i = 0; i < ac; ++i) {
+		end = d::depth.upper_bound(i);
+		printf("key= %d\n", i);
+		for (d::multimap::iterator beg = d::depth.lower_bound(i);
+				beg != end; ++beg) {
+			printf("value= %d\n", beg->second);
+		}
+		puts("-----");
+	}
 	return 0;
 }
